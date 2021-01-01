c8b6b49076fb7e637f58970cba74b93852631780
gulbahar ceferova <jafarova2020@gmail.com>
2021-01-01 20:50:52 +0400

    "01/01/2021" merge olmuş variant

--- public_html/resource/js/link.js
+++ public_html/resource/js/link.js
@@ -7,16 +7,12 @@
 
 
 function getToken() {
-    return  "apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.mjCmSGzzUBYZRaTfjCmhRgI3DiKQFWfgMqWR9_xobgXIUh_z8o3EmA.0mmH0xF7DaavC-wR3F80Vw.EyvIrmG0_wBRm_ZgvPF1k-udJi1YK1m-FYfOCxmERYPhhTDv2GtfrpTmHG-7vSr8NqsM0Khv8AGbLT_vDTnzeVseGPVBwwrKyEWY7jB2U3vLadFOKwJA191axyIoAA8mp0zsV2i7i_NXIamWZ_5V69II5JjbPyTnM3cd-dRjPlI.-OZX47q5cidLZW2PGtQmdA"
-//    return  "apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.3GJXeHFi_RKCskoP58QOR-GNlJITQS2HuFkUHbZ5hvHtxZ8IfA5FIw.dyIy_rwz6fQGwkZlz9z32g.1S5Fw38RAnAQJDnd7ZqoHTexAtVgqByuNTt3FaUI_UKB-ukPg0G6TqIDWa4uuZLBnVwlpci_17vPwdIRqcc5Bk3lTLsBMMnfwuWtv_Dxfqx7rSWiioSROgTwLmPEiYeIde6J9rFWYpfj07I5vmPeSOOWUn71mMNezmcFZWvQc6M.FQgGtlC2gn-OCT-p1uaAbw"
-//    return '';
+    return  "apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.ea3NSEIJJz7zanAVRItYHg1P32UHn9Ala2kpyimhNsiIKJyQXiWfbQ.EDzE-0AzMVfRJfESkbiapA.qPRbVwx6kKmrINZOEoYM2mloL6jYYrRDzWTjIcBrMmu7TSICfg1X6hH2vTQxjjM8-9KZiolQLByKtjcaw47jlG2p_oZjJhVfcnS4TY6vMOKHsoCcszmUhTNjGrCNkg_wVZSa9OlNkwEd0_xdEMUh4Id5fdKvSpklJ0l42AINiPY.iF1aaWc-yXgPY0itoY96Ug"
 }
 
-function getTokenDiff() {
-  return  "apdtok=eyJhbGciOiJBMTI4S1ciLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.mjCmSGzzUBYZRaTfjCmhRgI3DiKQFWfgMqWR9_xobgXIUh_z8o3EmA.0mmH0xF7DaavC-wR3F80Vw.EyvIrmG0_wBRm_ZgvPF1k-udJi1YK1m-FYfOCxmERYPhhTDv2GtfrpTmHG-7vSr8NqsM0Khv8AGbLT_vDTnzeVseGPVBwwrKyEWY7jB2U3vLadFOKwJA191axyIoAA8mp0zsV2i7i_NXIamWZ_5V69II5JjbPyTnM3cd-dRjPlI.-OZX47q5cidLZW2PGtQmdA"
-}
-//var urlGl = "https://app.apdvoice.com/";
-var urlGl = "http://localhost:8080/apd1/";
+
+var urlGl = "https://app.apdvoice.com/";
+//var urlGl = "http://localhost:8080/apd1/";
 //var urlGl = ""
 
 
